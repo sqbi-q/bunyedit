3f2aadf6c30de1b61b3f1020fcd18c3b7c055b0a
sqbi <sebasqbi@gmail.com>
2025-07-09 03:05:33 +0200

    Fix tile entity set

--- src/main/java/fun/raccoon/bunyedit/data/buffer/BlockBuffer.java
+++ src/main/java/fun/raccoon/bunyedit/data/buffer/BlockBuffer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import net.minecraft.core.block.Block;
 import net.minecraft.core.block.entity.TileEntity;
 import net.minecraft.core.enums.LightLayer;
 import net.minecraft.core.util.collection.Pair;
@@ -21,20 +22,30 @@ public class BlockBuffer extends HashMap<ChunkPosition, BlockData> {
     public void placeRaw(World world, ChunkPosition pos, BlockData blockData) {
         this.put(pos, blockData);
 
-        
-
+        // TODO inform player that coords are outside bounds
         world.setBlockRaw(pos.x, pos.y, pos.z, blockData.id);
         world.setBlockMetadata(pos.x, pos.y, pos.z, blockData.meta);
-        if (blockData.nbt != null) {
-            TileEntity entityFromNBT = new TileEntity();
-            entityFromNBT.readFromNBT(blockData.nbt);
 
-            world.setTileEntity(
-                pos.x, pos.y, pos.z,
-                // TileEntity.createAndLoadEntity(blockData.nbt)
-                entityFromNBT
-            );
+        if (blockData.nbt == null) {
+            // clean any tile entity left there
+            world.removeBlockTileEntity(pos.x, pos.y, pos.z);
+            return;
         }
+
+        Block<?> asBlock = blockData.getBlock();
+        if (asBlock == null)                return; // air.
+        if (asBlock.entitySupplier == null) return; // no tile entity supplier
+        
+        // Prefer block position over read blockData.nbt xyz position, but so 
+        // far it doesn't cause any problem. Need to look how NBTs look after save
+        TileEntity entityFromBlock = asBlock.entitySupplier.get();
+        entityFromBlock.readFromNBT(blockData.nbt);
+
+        world.setTileEntity(
+            pos.x, pos.y, pos.z,
+            // TileEntity.createAndLoadEntity(blockData.nbt)
+            entityFromBlock
+        );
     }
 
     public void finalize(World world) {
